e8c28c86f94abc2c96d9939295bbd7cba9baf75f
saiprasadkilaru <kilarusaiprasad19@gmail.com>
2024-10-02 17:15:10 +0530

    committed

---
+++ restapi1/src/main/java/com/restapi1/Restapi1Application.java
@@ -0,0 +1,15 @@
+package com.restapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Restapi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restapi1Application.class, args);
+	}
+
+}
